5aea02a05a073165f40ebe94afb9c9073b16e764
Daniel Katz <dgkatz@Daniels-MacBook-Pro.local>
2022-05-23 17:36:08 -0400

    Bump http session max connections used by rpc provider

--- ethereumetl/providers/base.py
+++ ethereumetl/providers/base.py
@@ -1,9 +1,14 @@
+import requests
+import requests.adapters
 from web3.utils.request import _get_session
 
 
 def make_post_request(endpoint_uri, data, args: list = None, kwargs: dict = None):
     kwargs.setdefault('timeout', 10)
-    session = _get_session(endpoint_uri)
+    session: requests.Session = _get_session(endpoint_uri)
+    adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=100)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
     args = args or []
     response = session.post(endpoint_uri, data=data, *args, **kwargs)
     response.raise_for_status()
