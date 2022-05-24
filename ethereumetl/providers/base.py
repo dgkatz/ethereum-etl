f1ac3f4a55c180a5615585baf5865431ac83f46e
Daniel Katz <dgkatz@Daniels-MacBook-Pro.local>
2022-05-24 16:54:11 -0400

    fix session cache for rpc provider

--- ethereumetl/providers/base.py
+++ ethereumetl/providers/base.py
@@ -1,14 +1,32 @@
+import lru
 import requests
 import requests.adapters
-from web3.utils.request import _get_session
+from web3.utils.caching import (
+    generate_cache_key,
+)
+
+
+def _remove_session(key, session):
+    session.close()
+
+
+_session_cache = lru.LRU(8, callback=_remove_session)
+
+
+def _get_session(*args, **kwargs):
+    cache_key = generate_cache_key((args, kwargs))
+    if cache_key not in _session_cache:
+        session = requests.Session()
+        adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=100)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+        _session_cache[cache_key] = session
+    return _session_cache[cache_key]
 
 
 def make_post_request(endpoint_uri, data, args: list = None, kwargs: dict = None):
     kwargs.setdefault('timeout', 10)
     session: requests.Session = _get_session(endpoint_uri)
-    adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=100)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
     args = args or []
     response = session.post(endpoint_uri, data=data, *args, **kwargs)
     response.raise_for_status()
